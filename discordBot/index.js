5c6898839493eb234d0c9a8fbcfb075b0fe8908d
Layan Anabtawi <noatttentionuser@outlook.com>
2025-05-01 12:55:18 +0100

    Added author recognition

--- discordBot/index.js
+++ discordBot/index.js
@@ -1,24 +1,40 @@
-require('dotenv').config();
+require('dotenv').config();//config reads the .env file and loads into process.env
 
+//import discord.js
+//clinet is the bot class
+//gataway intent is the constants to say what type of event sto listen to 
 const {Client, GatewayIntentBits} = require("discord.js") //needs intents 
-
+/*
+const discordJS = require("discord.js");
+const Client = discordJS.Client;
+const GatewayIntentBits = discordJS.GatewayIntentBits;
+*/
+//say what i want to listen to 
+//the cline tobject is the bot and it talks between bot and discord server
 const client = new Client({
     intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
+        GatewayIntentBits.Guilds,//like joining or leaving 
+        GatewayIntentBits.GuildMessages,//read mesages from server
+        GatewayIntentBits.MessageContent//read m conent 
       ]
     });
 
-
+//syntax and where does it logged in as 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 });
 
 client.on("messageCreate", msg => {
-  if (msg.content === "i love my") {
-    msg.reply("bestest boyfriend in the world mwaah");
+  if (msg.content === "i love my" && msg.author.bot == "wrathwrath")  {
+    msg.reply("gf");
   }
+  else if (msg.content === "i love my" && msg.author.bot == "nemeton5511")  {
+    msg.reply("bf");
+  }
+  else {
+    msg.reply("ERROR");
+  }
+
 });
 
 client.login(process.env.TOKEN)
